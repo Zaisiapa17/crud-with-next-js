bee0ae4f651faa292551d29a642f0ff7c2a262a5
alie <mushyafaali@gmail.com>
2024-02-05 10:03:32 +0700

    add react-icons

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTask = () => {
+    return (
+        <>
+            <button className="btn btn-neutral">Neutral</button>
+        </>
+    )
+}
+
+export default AddTask
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import AddTask from "./components/AddTask";
 
 export default function Home() {
     return (
-        <main className="max-w-4xl mx-auto mt-4 bg-white">
-            <div>
-                testing
+        <main className="w-10/12 mx-auto mt-4">
+            <div className='text-center my-5 flex flex-col gap-4'>
+                <h1 className='text-2xl font-bold'>Todo List App</h1>
+                <AddTask />
             </div>
+            {/* <TodoList tasks={tasks} /> */}
         </main>
     );
 }
